af09abfae8c991f713dd67c6cd71eb06089c2c1f
Anik Bhattacharya <anik.bhattacharya@hcss.com>
2021-01-24 03:01:48 -0600

    create express server starting script

--- scripts/server.js
+++ scripts/server.js
@@ -7,18 +7,18 @@ const bodyParser = require('body-parser');
 // Build server
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../public')));
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+app.get('/test', function (req, res) {
+  return res.send("Hello World!");
 });
 
 // Run server
 app.listen(process.env.PORT || 8080, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
   console.log(
-    `%cServer started successfully; listening on http://${host}:${port}`,
+    `%cServer started successfully`,
     "color: lime"
   );
 });

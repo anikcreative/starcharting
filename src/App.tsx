c4e08f47d67dab5903919e7a985eadf47294f7f9
Anik Bhattacharya <anik.bhattacharya@hcss.com>
2021-01-24 01:29:03 -0600

    clean up unneeded files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app-base">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>

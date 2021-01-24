efc66dbaaf04aae84214253574f0dca39439830d
Anik Bhattacharya <anik.bhattacharya@hcss.com>
2021-01-24 01:38:52 -0600

    clean up jest test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  describe("Main client application should...", () => {
+    it("render without crashing", async () => {
+      const { findByText } = render(<App />);
+
+      const comingSoonMessage = "Coming soon!";
+      const element = await findByText(comingSoonMessage);
+      expect(element).toBeDefined();
+    });
+  });
 });
